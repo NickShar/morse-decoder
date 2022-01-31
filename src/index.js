bf2dd46c0f5a8b3402c2bf68290984879691562b
Zenbook <sharnin@urbis.by>
2022-01-31 14:14:02 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
+    const arr = expr.split("");
+    const code = [];
+    for (let i = 0; i < arr.length; i += 10) {
+      code.push(arr.slice(i, i + 10));
+    }
+    const transarr = code.map(x => x.join("").match(/(11|10)/gm)).map(x => x === null ? " " : x.map(y => +y === 10 ? "." : "-"));
+    let result = '';
+    transarr.map(x => x === " " ? (result += " ") : (result += MORSE_TABLE[x.join("")]));
+    return result;
+  }
 module.exports = {
     decode
 }
